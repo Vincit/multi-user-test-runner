4280420a0a2fb530e32f4a1f5fbd607dcaf8dee8
Juha Siponen <juha.siponen@vincit.fi>
2015-05-15 18:14:59 +0300

    Fix pipeline counter to build script

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION_BUILD_NUMBER=".${COUNT}"
+VERSION_BUILD_NUMBER=".${GO_PIPELINE_COUNTER}"
 echo "Building with build no. ${VERSION_BUILD_NUMBER}"
 
 mvn clean test package -DbuildNumber="${VERSION_BUILD_NUMBER}"
