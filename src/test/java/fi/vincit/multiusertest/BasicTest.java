d41176f09af41d54654312dc1de95c8ce4213b80
Juha Siponen <juha.siponen@vincit.fi>
2015-07-24 19:42:49 +0300

    Change tests to use new assertion syntax

--- src/test/java/fi/vincit/multiusertest/BasicTest.java
+++ src/test/java/fi/vincit/multiusertest/BasicTest.java
@@ -1,18 +1,23 @@
 package fi.vincit.multiusertest;
 
-import fi.vincit.multiusertest.annotation.TestUsers;
-import fi.vincit.multiusertest.configuration.ConfiguredTest;
-import fi.vincit.multiusertest.runner.BlockMultiUserTestClassRunner;
-import fi.vincit.multiusertest.runner.MultiUserTestRunner;
-import fi.vincit.multiusertest.util.*;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.security.access.AccessDeniedException;
-
+import static fi.vincit.multiusertest.rule.Authentication.ifAnyOf;
 import static fi.vincit.multiusertest.rule.Authentication.notToFail;
 import static fi.vincit.multiusertest.rule.Authentication.toFail;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.security.access.AccessDeniedException;
+
+import fi.vincit.multiusertest.annotation.TestUsers;
+import fi.vincit.multiusertest.configuration.ConfiguredTest;
+import fi.vincit.multiusertest.runner.junit.framework.BlockMultiUserTestClassRunner;
+import fi.vincit.multiusertest.runner.junit.MultiUserTestRunner;
+import fi.vincit.multiusertest.util.LoginRole;
+import fi.vincit.multiusertest.util.SecurityUtil;
+import fi.vincit.multiusertest.util.User;
+import fi.vincit.multiusertest.util.UserIdentifier;
 
 @TestUsers(creators = {"role:ROLE_ADMIN", "role:ROLE_USER"},
         users = {"role:ROLE_ADMIN", "role:ROLE_USER"},
@@ -40,32 +45,32 @@ public class BasicTest extends ConfiguredTest {
 
     @Test
     public void expectFailureCreator() {
-        authorization().expect(toFail().ifAnyOf(TestUsers.CREATOR));
+        authorization().expect(toFail(ifAnyOf(TestUsers.CREATOR)));
         throwIfUserIs(getCreator());
     }
 
     @Test
     public void expectFailureNewUser() {
-        authorization().expect(toFail().ifAnyOf(TestUsers.NEW_USER));
+        authorization().expect(toFail(ifAnyOf(TestUsers.NEW_USER)));
         throwIfUserIs(getUser());
     }
 
     @Test
     public void expectFailureNotCreator() {
-        authorization().expect(notToFail().ifAnyOf(TestUsers.CREATOR));
+        authorization().expect(notToFail(ifAnyOf(TestUsers.CREATOR)));
         throwIfUserIs(getUser());
     }
 
     @Test
     public void expectFailureNotNewUser() {
-        authorization().expect(notToFail().ifAnyOf(TestUsers.NEW_USER));
+        authorization().expect(notToFail(ifAnyOf(TestUsers.NEW_USER)));
         throwIfUserIs(getCreator());
     }
 
     @Test
     public void expectFailureUser() {
         logInAs(LoginRole.USER);
-        authorization().expect(toFail().ifAnyOf("role:ROLE_USER"));
+        authorization().expect(toFail(ifAnyOf("role:ROLE_USER")));
         throwIfUserRole("role:ROLE_USER");
     }
 
--- src/test/java/fi/vincit/multiusertest/SmokeTest.java
+++ src/test/java/fi/vincit/multiusertest/SmokeTest.java
@@ -1,17 +1,21 @@
 package fi.vincit.multiusertest;
 
-import fi.vincit.multiusertest.annotation.TestUsers;
-import fi.vincit.multiusertest.configuration.ConfiguredTest;
-import fi.vincit.multiusertest.rule.Authentication;
-import fi.vincit.multiusertest.rule.AuthorizationRule;
-import fi.vincit.multiusertest.runner.BlockMultiUserTestClassRunner;
-import fi.vincit.multiusertest.runner.MultiUserTestRunner;
-import fi.vincit.multiusertest.util.LoginRole;
+import static fi.vincit.multiusertest.rule.Authentication.ifAnyOf;
+import static fi.vincit.multiusertest.rule.Authentication.notToFail;
+import static fi.vincit.multiusertest.rule.Authentication.toFail;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.RuleChain;
 import org.junit.runner.RunWith;
+
+import fi.vincit.multiusertest.annotation.TestUsers;
+import fi.vincit.multiusertest.configuration.ConfiguredTest;
+import fi.vincit.multiusertest.rule.AuthorizationRule;
+import fi.vincit.multiusertest.runner.junit.framework.BlockMultiUserTestClassRunner;
+import fi.vincit.multiusertest.runner.junit.MultiUserTestRunner;
+import fi.vincit.multiusertest.util.LoginRole;
 
 @TestUsers(creators = {"role:ROLE_ADMIN"}, users = "role:ROLE_ADMIN",
         runner = BlockMultiUserTestClassRunner.class)
@@ -30,13 +34,13 @@ public class SmokeTest extends ConfiguredTest {
     @Test
     public void passes() {
         logInAs(LoginRole.USER);
-        authorization().expect(Authentication.notToFail().ifAnyOf("role:ROLE_ADMIN"));
+        authorization().expect(notToFail(ifAnyOf("role:ROLE_ADMIN")));
     }
 
     @Test
     public void fails() {
         expectedException.expect(AssertionError.class);
         logInAs(LoginRole.USER);
-        expectFailAuthRule.expect(Authentication.toFail().ifAnyOf("role:ROLE_ADMIN"));
+        expectFailAuthRule.expect(toFail(ifAnyOf("role:ROLE_ADMIN")));
     }
 }
