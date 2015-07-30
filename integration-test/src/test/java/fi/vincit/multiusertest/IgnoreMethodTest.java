9ccbfe5123f7218d7f590c459c7a01ccc8e41bd8
Juha Siponen <juha.siponen@vincit.fi>
2015-07-30 20:05:34 +0300

    Add assertions to IgnoreMethodTest

--- integration-test/src/test/java/fi/vincit/multiusertest/IgnoreMethodTest.java
+++ integration-test/src/test/java/fi/vincit/multiusertest/IgnoreMethodTest.java
@@ -1,12 +1,16 @@
 package fi.vincit.multiusertest;
 
-import fi.vincit.multiusertest.annotation.TestUsers;
-import fi.vincit.multiusertest.configuration.ConfiguredTest;
-import fi.vincit.multiusertest.runner.junit.framework.BlockMultiUserTestClassRunner;
-import fi.vincit.multiusertest.runner.junit.MultiUserTestRunner;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import fi.vincit.multiusertest.annotation.TestUsers;
+import fi.vincit.multiusertest.configuration.ConfiguredTest;
+import fi.vincit.multiusertest.runner.junit.MultiUserTestRunner;
+import fi.vincit.multiusertest.runner.junit.framework.BlockMultiUserTestClassRunner;
+import fi.vincit.multiusertest.util.User;
 
 @TestUsers(creators = {"role:ROLE_ADMIN", "role:ROLE_USER"},
         users = {"role:ROLE_ADMIN", "role:ROLE_USER"},
@@ -14,25 +18,36 @@ import org.junit.runner.RunWith;
 @RunWith(MultiUserTestRunner.class)
 public class IgnoreMethodTest extends ConfiguredTest {
 
-
     @Test
     @TestUsers(creators = {"role:ROLE_ADMIN"})
     public void runCreatorAdmin() {
+        assertThat(getCreatorRole(), is(User.Role.ROLE_ADMIN));
     }
 
     @Test
     @TestUsers(users = {"role:ROLE_USER"})
     public void runUserIsUser() {
+        assertThat(getUserRole(), is(User.Role.ROLE_USER));
     }
 
     @Test
     @TestUsers(users = {"role:ROLE_ADMIN"}, creators = {"role:ROLE_ADMIN"})
     public void runUserAdminAndCreatorAdmin() {
+        assertThat(getUserRole(), is(User.Role.ROLE_ADMIN));
+        assertThat(getCreatorRole(), is(User.Role.ROLE_ADMIN));
     }
 
     @Test
     @TestUsers(users = {"role:ROLE_USER"}, creators = {"role:ROLE_ADMIN"})
     public void runUserUserAndCreatorAdmin() {
+        assertThat(getCreatorRole(), is(User.Role.ROLE_ADMIN));
+        assertThat(getUserRole(), is(User.Role.ROLE_USER));
+    }
+
+    @Test
+    @TestUsers(users = {"role:ROLE_VISITOR"}, creators = {"role:ROLE_VISITOR"})
+    public void neverRun() {
+        throw new AssertionError("Should never call this method");
     }
 
 }
