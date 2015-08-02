b9ab63fc0bea683cc2453967fa361069867935b1
Juha Siponen <juha.siponen@vincit.fi>
2015-08-02 19:38:31 +0300

    Remove spring-test running from build-and-package scripts
    Spring tests are run separately in own pipeline now

--- build/test-and-package-all.sh
+++ build/test-and-package-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean test package -Dglobal.version=${GO_PIPELINE_LABEL}
+mvn clean test package -Dglobal.version=${GO_PIPELINE_LABEL} --projects core,integration-test,java-8-test
--- build/test-and-package-java-7.sh
+++ build/test-and-package-java-7.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean test package -pl core,integration-test -Dglobal.version=${GO_PIPELINE_LABEL}
+mvn clean test package -pl core,integration-test -Dglobal.version=${GO_PIPELINE_LABEL} --projects core,integration-test
