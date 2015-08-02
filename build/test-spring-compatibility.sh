b77b569d44d1dc7bd8b35b0b77d50a3f6cd011d3
Juha Siponen <juha.siponen@vincit.fi>
2015-08-02 19:15:36 +0300

    Add support for defining SUT version to test against
    Makes possible to define SUT (System Under Test, multi-user-test-runner)
    so that it is possible to test without building multiple times

--- build/test-spring-compatibility.sh
+++ build/test-spring-compatibility.sh
@@ -4,7 +4,14 @@ SPRING_MAJOR_VERSION=$1
 SPRING_VERSION="${SPRING_MAJOR_VERSION}"
 SPRING_FOLDER="spring-test"
 
-echo "Testing against ${SPRING_VERSION}"
+SUT_VERSION=$2
+
+echo "Testing against: "
+echo " Spring version: ${SPRING_VERSION}"
+echo " multi-user-test-runner: ${SUT_VERSION}"
 echo " in folder ./${SPRING_FOLDER}"
 
-mvn clean test -Dglobal.spring-version=${SPRING_VERSION} --projects core,spring-test
+mvn clean test \
+    -Dglobal.spring-version=${SPRING_VERSION} \
+    -Dglobal.version=${SUT_VERSION} \
+    --projects spring-test
