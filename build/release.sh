a23d4cb574eabfb139eef5c6ce2c513dd4c07531
Juha Siponen <juha.siponen@vincit.fi>
2015-08-16 19:19:48 +0300

    Add SCM username and password to release script

--- build/release.sh
+++ build/release.sh
@@ -3,8 +3,10 @@
 RELEASE_VERSION=${GO_PIPELINE_LABEL}
 DEVEL_VERSION="0.2-SNAPSHOT"
 
-mvn release:prepare-with-pom --projects .,core --batch-mode \
+mvn release:prepare-with-pom --batch-mode \
  -Dtag=${RELEASE_VERSION} \
  -DreleaseVersion=${RELEASE_VERSION} \
  -DdevelopmentVersion=${DEVEL_VERSION} \
+ -Dpassword=${SCM_PASSWORD} \
+ -Dusername=${SCM_USERNAME} \
  -e
