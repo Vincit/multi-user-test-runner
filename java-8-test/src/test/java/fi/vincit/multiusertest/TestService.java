dc57d0d8dce953b35751d4647d7c5ce26d2d33e4
Juha Siponen <juha.siponen@vincit.fi>
2015-08-23 23:55:23 +0300

    Remove Spring dependencies from Java 8 test

--- java-8-test/src/test/java/fi/vincit/multiusertest/TestService.java
+++ java-8-test/src/test/java/fi/vincit/multiusertest/TestService.java
@@ -1,10 +1,8 @@
 package fi.vincit.multiusertest;
 
-import org.springframework.security.access.AccessDeniedException;
-
 public class TestService {
     public void throwAccessDenied() {
-        throw new AccessDeniedException("Denied");
+        throw new IllegalStateException("Denied");
     }
 
     public void noThrow() {
