d1458cea9c306a5565f681ec8d0a0558098be0f0
Juha Siponen <juha.siponen@vincit.fi>
2015-10-20 23:30:36 +0300

    Refactor UserResolver

--- core/src/main/java/fi/vincit/multiusertest/test/UserResolver.java
+++ core/src/main/java/fi/vincit/multiusertest/test/UserResolver.java
@@ -6,18 +6,20 @@ import fi.vincit.multiusertest.util.UserIdentifier;
 
 public class UserResolver<USER, ROLE> {
 
-    private final TestUser<ROLE> creator;
-    private USER creatorUser;
-    private final TestUser<ROLE> user;
-    private USER userUser;
+    private final TestUser<ROLE> creatorModel;
+    private USER creator;
+
+    private final TestUser<ROLE> userModel;
+    private USER user;
+
     private final UserFactory<USER, ROLE> userFactory;
     private final RoleConverter<ROLE> roleConverter;
 
     public UserResolver(UserFactory<USER, ROLE> userFactory, RoleConverter<ROLE> roleConverter, UserIdentifier creator, UserIdentifier user) {
         this.userFactory = userFactory;
         this.roleConverter = roleConverter;
-        this.creator = resolveCreatorFromIdentifier(creator);
-        this.user = resolveUserFromIdentifier(user);
+        this.creatorModel = resolveCreatorFromIdentifier(creator);
+        this.userModel = resolveUserFromIdentifier(user);
     }
 
     private TestUser<ROLE> resolveCreatorFromIdentifier(UserIdentifier identifier) {
@@ -39,10 +41,10 @@ public class UserResolver<USER, ROLE> {
         if (identifier.getType() == UserIdentifier.Type.CREATOR) {
             return TestUser.forCreatorUser();
         } else if (identifier.getType() == UserIdentifier.Type.NEW_USER) {
-            if (this.creator.getMode() == TestUser.RoleMode.EXISTING_USER) {
+            if (creatorModel.getMode() == TestUser.RoleMode.EXISTING_USER) {
                 throw new IllegalStateException("Cannot use NEW_USER mode when creator uses existing user");
             }
-            return TestUser.forNewUser(creator.getRole(), identifier);
+            return TestUser.forNewUser(creatorModel.getRole(), identifier);
         } else if (identifier.getType() == UserIdentifier.Type.ANONYMOUS) {
             return TestUser.forAnonymousUser();
         } else if (identifier.getType() == UserIdentifier.Type.ROLE) {
@@ -52,74 +54,74 @@ public class UserResolver<USER, ROLE> {
         }
     }
 
-    private void initializeUser(TestUser<ROLE> user, UserFactory<USER, ROLE> userFactory) {
-        if (user.getMode() == TestUser.RoleMode.SET_USER_ROLE) {
-            userUser = userFactory.createUser(userFactory.getRandomUsername(), "Test", "User", user.getRole(), LoginRole.USER);
-        } else if (user.getMode() == TestUser.RoleMode.CREATOR_USER) {
-            if (creator.getMode() == TestUser.RoleMode.EXISTING_USER) {
-                // Do nothing, user already set, using creator
+    private void initializeUser() {
+        if (userModel.getMode() == TestUser.RoleMode.SET_USER_ROLE) {
+            user = userFactory.createUser(userFactory.getRandomUsername(), "Test", "User", userModel.getRole(), LoginRole.USER);
+        } else if (userModel.getMode() == TestUser.RoleMode.CREATOR_USER) {
+            if (creatorModel.getMode() == TestUser.RoleMode.EXISTING_USER) {
+                // Do nothing, resolved in getter
             } else {
-                userUser = creatorUser;
+                user = creator;
             }
-        } else if (user.getMode() == TestUser.RoleMode.NEW_WITH_CREATOR_ROLE) {
-            if (creator.getMode() == TestUser.RoleMode.EXISTING_USER) {
+        } else if (userModel.getMode() == TestUser.RoleMode.NEW_WITH_CREATOR_ROLE) {
+            if (creatorModel.getMode() == TestUser.RoleMode.EXISTING_USER) {
                 // NOOP
             } else {
-                userUser = userFactory.createUser(userFactory.getRandomUsername(), "Test", "User", creator.getRole(), LoginRole.USER);
+                user = userFactory.createUser(userFactory.getRandomUsername(), "Test", "User", creatorModel.getRole(), LoginRole.USER);
             }
-        } else if (user.getMode() == TestUser.RoleMode.EXISTING_USER) {
-            // Do nothing, user already set
-        } else if (user.getMode() == TestUser.RoleMode.ANONYMOUS) {
+        } else if (userModel.getMode() == TestUser.RoleMode.EXISTING_USER) {
+            // Do nothing, resolved in getter
+        } else if (userModel.getMode() == TestUser.RoleMode.ANONYMOUS) {
             // Do nothing, user is not used
         } else {
-            throw new IllegalArgumentException("Invalid user mode: " + user.getMode());
+            throw new IllegalArgumentException("Invalid user mode: " + userModel.getMode());
         }
     }
 
-    private void initializeCreator(TestUser<ROLE> creator, UserFactory<USER, ROLE> userFactory) {
-        if (creator.getMode() == TestUser.RoleMode.SET_USER_ROLE) {
-            creatorUser = userFactory.createUser(userFactory.getRandomUsername(), "Test", "Creator", creator.getRole(), LoginRole.CREATOR);
-        } else if (creator.getMode() == TestUser.RoleMode.EXISTING_USER) {
-            // Do nothing, user already set
-        } else if (creator.getMode() == TestUser.RoleMode.ANONYMOUS) {
+    private void initializeCreator() {
+        if (creatorModel.getMode() == TestUser.RoleMode.SET_USER_ROLE) {
+            creator = userFactory.createUser(userFactory.getRandomUsername(), "Test", "Creator", creatorModel.getRole(), LoginRole.CREATOR);
+        } else if (creatorModel.getMode() == TestUser.RoleMode.EXISTING_USER) {
+            // Do nothing, resolved in getter
+        } else if (creatorModel.getMode() == TestUser.RoleMode.ANONYMOUS) {
             // Do nothing, user is not used
         } else {
-            throw new IllegalArgumentException("Invalid creator user mode: " + creator.getMode());
+            throw new IllegalArgumentException("Invalid creator user mode: " + creatorModel.getMode());
         }
     }
 
     public TestUser<ROLE> getCreator() {
-        return creator;
+        return creatorModel;
     }
 
     public TestUser<ROLE> getUser() {
-        return user;
+        return userModel;
     }
 
     public USER resolveUser() {
-        if (user.getMode() == TestUser.RoleMode.EXISTING_USER) {
-            return userFactory.getUserByUsername(user.getIdentifier());
-        } else if (user.getMode() == TestUser.RoleMode.ANONYMOUS) {
+        if (userModel.getMode() == TestUser.RoleMode.EXISTING_USER) {
+            return userFactory.getUserByUsername(userModel.getIdentifier());
+        } else if (userModel.getMode() == TestUser.RoleMode.ANONYMOUS) {
             return null;
-        } else if (user.getMode() == TestUser.RoleMode.CREATOR_USER) {
+        } else if (userModel.getMode() == TestUser.RoleMode.CREATOR_USER) {
             return resolverCreator();
         } else {
-            return userUser;
+            return user;
         }
     }
 
     public USER resolverCreator() {
-        if (creator.getMode() == TestUser.RoleMode.EXISTING_USER) {
-            return userFactory.getUserByUsername(creator.getIdentifier());
-        } else if (creator.getMode() == TestUser.RoleMode.ANONYMOUS) {
+        if (creatorModel.getMode() == TestUser.RoleMode.EXISTING_USER) {
+            return userFactory.getUserByUsername(creatorModel.getIdentifier());
+        } else if (creatorModel.getMode() == TestUser.RoleMode.ANONYMOUS) {
             return null;
         } else {
-            return creatorUser;
+            return creator;
         }
     }
 
     public void resolve() {
-        initializeCreator(creator, userFactory);
-        initializeUser(user, userFactory);
+        initializeCreator();
+        initializeUser();
     }
 }
