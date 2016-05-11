34bbaf725daef30430ca744ec39ae871b5e52607
Juha Siponen <juha.siponen@vincit.fi>
2016-05-11 23:19:52 +0300

    Fix component based initialization

--- core/src/main/java/fi/vincit/multiusertest/util/RunnerDelegate.java
+++ core/src/main/java/fi/vincit/multiusertest/util/RunnerDelegate.java
@@ -8,7 +8,10 @@ import fi.vincit.multiusertest.test.UserRoleIT;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.internal.runners.statements.RunBefores;
-import org.junit.runners.model.*;
+import org.junit.runners.model.FrameworkField;
+import org.junit.runners.model.FrameworkMethod;
+import org.junit.runners.model.Statement;
+import org.junit.runners.model.TestClass;
 
 import java.lang.reflect.Field;
 import java.util.List;
@@ -154,9 +157,10 @@ public class RunnerDelegate {
                         AuthorizationRule authorizationRule = getAuthorizationRule(testClass, target);
 
                         multiUserConfig.setAuthorizationRule(authorizationRule, target);
+                        multiUserConfig.initialize();
+                    } else {
+                        userRoleIt.logInAs(LoginRole.PRODUCER);
                     }
-
-                    userRoleIt.logInAs(LoginRole.PRODUCER);
                 }
             };
 
