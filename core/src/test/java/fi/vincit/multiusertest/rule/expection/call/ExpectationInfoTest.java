0b07f2f24928d5bffe42055bd0ce2094a7aff407
Juha Siponen <juha.siponen@vincit.fi>
2016-07-09 18:25:01 +0300

    Add more tests for ExcpectationInfo

--- core/src/test/java/fi/vincit/multiusertest/rule/expection/call/ExpectationInfoTest.java
+++ core/src/test/java/fi/vincit/multiusertest/rule/expection/call/ExpectationInfoTest.java
@@ -1,9 +1,14 @@
 package fi.vincit.multiusertest.rule.expection.call;
 
-import org.junit.Test;
-
 import fi.vincit.multiusertest.rule.FailMode;
 import fi.vincit.multiusertest.util.Optional;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class ExpectationInfoTest {
 
@@ -31,6 +36,35 @@ public class ExpectationInfoTest {
     @Test(expected = IllegalArgumentException.class)
     public void testValidationFail_FailModeNone_WithoutException() {
         new ExpectationInfo(FailMode.NONE, getEmptyException(), ExpectCall.NOOP_ASSERTION);
+    }
+
+    @Test
+    public void testIsExceptionExpected_ExpectNotToFail() {
+        ExpectationInfo expectationInfo = new ExpectationInfo(FailMode.EXPECT_NOT_FAIL, getEmptyException(), ExpectCall.NOOP_ASSERTION);
+        assertThat(expectationInfo.isExceptionExpected(new Throwable(), Throwable.class), is(false));
+    }
+
+    @Test
+    public void testIsExceptionExpected_ExpectToFail_DefaultException() {
+        ExpectationInfo expectationInfo = new ExpectationInfo(FailMode.EXPECT_FAIL, getEmptyException(), ExpectCall.NOOP_ASSERTION);
+        assertThat(expectationInfo.isExceptionExpected(new Throwable(), Throwable.class), is(true));
+    }
+
+    @Test
+    public void testIsExceptionExpected_ExpectToFail_GivenException() {
+        ExpectationInfo expectationInfo = new ExpectationInfo(FailMode.EXPECT_FAIL, getOptionalException(), ExpectCall.NOOP_ASSERTION);
+        assertThat(expectationInfo.isExceptionExpected(new Throwable(), RuntimeException.class), is(true));
+    }
+
+    @Test
+    public void testAssertExceptionCall() throws Throwable {
+        ExceptionAssertionCall call = mock(ExceptionAssertionCall.class);
+        ExpectationInfo expectationInfo = new ExpectationInfo(FailMode.EXPECT_FAIL, getOptionalException(), call);
+        Throwable exception = new Throwable();
+
+        expectationInfo.assertException(exception);
+
+        verify(call).assertException(eq(exception));
     }
 
     private Optional<Class<? extends Throwable>> getOptionalException() {
