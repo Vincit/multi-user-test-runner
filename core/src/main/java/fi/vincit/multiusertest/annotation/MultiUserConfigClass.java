122aa6b79a23e1d15ce5c1c32bafecdba796ba4c
Juha Siponen <juha.siponen@vincit.fi>
2018-03-03 22:34:56 +0200

    Add JavaDoc for MultiUserConfigClass annotation

--- core/src/main/java/fi/vincit/multiusertest/annotation/MultiUserConfigClass.java
+++ core/src/main/java/fi/vincit/multiusertest/annotation/MultiUserConfigClass.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks the property for the test class' configuration. The configuration
+ * class has to extend the {@link fi.vincit.multiusertest.test.AbstractMultiUserConfig}
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD})
 public @interface MultiUserConfigClass {
