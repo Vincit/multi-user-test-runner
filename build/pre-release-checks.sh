81586446401b3e04db7a537f8742dd845c572a65
Juha Siponen <juha.siponen@vincit.fi>
2021-09-22 21:23:53 +0300

    Fix release scripts to work on local machine

--- build/pre-release-checks.sh
+++ build/pre-release-checks.sh
@@ -2,15 +2,17 @@
 
 README_FILE="README.md"
 RETURN_VALUE=0
-RELEASE_VERSION=${cat version}
+RELEASE_VERSION=$(cat version)
 
-echo "Checking Maven example version. Expecting version $RELEASE_VERSION"
+echo "Checking Maven example version. Expecting version \"$RELEASE_VERSION\""
 MAVEN_VERSION_COUNT=$(grep "<version>$RELEASE_VERSION</version>" $README_FILE | wc -l)
 
 if [ $MAVEN_VERSION_COUNT -ne 1 ];
 then
     echo " *** Wrong version for Maven example, found $MAVEN_VERSION_COUNT"
     RETURN_VALUE=1
+else
+    echo " * Maven example version OK"
 fi
 
 echo "Checking Gradle example version"
@@ -21,6 +23,8 @@ if [ $GRADLE_VERSION_COUNT_CORE -ne "1" ];
 then
     echo " *** Wrong version for fi.vincit:multi-user-test-runner, found $GRADLE_VERSION_COUNT_CORE"
     RETURN_VALUE=1
+else
+    echo " * Gradle example version OK"
 fi
 
 exit $RETURN_VALUE
--- build/release-docs.sh
+++ build/release-docs.sh
@@ -2,13 +2,11 @@
 
 echo "Releasing documentation"
 
-RELEASE_VERSION=${cat version}
-DOCS_FULL_URL="https://${DOCS_USERNAME}:${DOCS_PASSWORD}@${DOCS_GIT_REPO}"
+RELEASE_VERSION=$(cat version)
+DOCS_FULL_URL="ssh://git@${DOCS_GIT_REPO}"
 DOCS_BRANCH=gh-pages
 DOCS_DIR=docs
 PROJECT_DIR=../project
-CURRENT_BRANCH=$GO_SCM_MUTR_RELEASE_SOURCE_CURRENT_BRANCH
-STABLE_BRANCH="master"
 
 echo " * Generate JavaDoc for version $RELEASE_VERSION"
 cd project
@@ -27,30 +25,22 @@ echo " * Copying JavaDocs to $JAVA_VERSION_DOC_DIR"
 mkdir -p $JAVA_VERSION_DOC_DIR
 cp -r $PROJECT_DIR/core/build/docs/javadoc/** $JAVA_VERSION_DOC_DIR
 
-if [ "$CURRENT_BRANCH" == "$STABLE_BRANCH" ]
-then
-    echo " * Copying JavaDocs to $JAVA_LATEST_DOC_DIR"
-    mkdir -p $JAVA_LATEST_DOC_DIR
-    rm -rf $JAVA_LATEST_DOC_DIR/*
-    cp -r $PROJECT_DIR/core/build/docs/javadoc/** $JAVA_LATEST_DOC_DIR
-else
-    echo " * Not $STABLE_BRANCH branch, skip release latest JavaDoc"
-fi
+
+echo " * Copying JavaDocs to $JAVA_LATEST_DOC_DIR"
+mkdir -p $JAVA_LATEST_DOC_DIR
+rm -rf $JAVA_LATEST_DOC_DIR/*
+cp -r $PROJECT_DIR/core/build/docs/javadoc/** $JAVA_LATEST_DOC_DIR
+
 # Copy README.md
 README_FILE="README.md"
 CHANGELOG_FILE="CHANGELOG.md"
 LATEST_DIR="_includes/latest"
 VERSION_DIR="_includes/$VERSION"
 
-if [ "$CURRENT_BRANCH" == "$STABLE_BRANCH" ]
-then
-    echo " * Copying $README_FILE to $LATEST_DIR"
-    mkdir -p LATEST_DIR
-    cp $PROJECT_DIR/$README_FILE $LATEST_DIR/$README_FILE
-    cp $PROJECT_DIR/$CHANGELOG_FILE $LATEST_DIR/$CHANGELOG_FILE
-else
-    echo " * Not $STABLE_BRANCH branch, skip release latest readme"
-fi
+echo " * Copying $README_FILE to $LATEST_DIR"
+mkdir -p LATEST_DIR
+cp $PROJECT_DIR/$README_FILE $LATEST_DIR/$README_FILE
+cp $PROJECT_DIR/$CHANGELOG_FILE $LATEST_DIR/$CHANGELOG_FILE
 
 echo " * Copying $README_FILE to $VERSION_DIR"
 mkdir -p $VERSION_DIR
@@ -61,12 +51,9 @@ echo " * Pusing to site"
 git add $JAVA_VERSION_DOC_DIR/**
 git add $VERSION_DIR/$README_FILE
 
-if [ "$CURRENT_BRANCH" == "$STABLE_BRANCH" ]
-then
-    git add $JAVA_LATEST_DOC_DIR/**
-    git add $LATEST_DIR/$README_FILE
-    git add $LATEST_DIR/$CHANGELOG_FILE
-fi
+git add $JAVA_LATEST_DOC_DIR/**
+git add $LATEST_DIR/$README_FILE
+git add $LATEST_DIR/$CHANGELOG_FILE
 
 git commit -m "Release $VERSION docs"
 
